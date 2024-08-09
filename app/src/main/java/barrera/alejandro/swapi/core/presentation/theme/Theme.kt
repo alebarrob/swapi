c44eff99a3dffce8c851fdc960393c4d400a6ce3
alebarrob <abarrobles@gmail.com>
2024-08-09 22:34:25 +0200

    Auto-generated code cleanup

--- app/src/main/java/barrera/alejandro/swapi/core/presentation/theme/Theme.kt
+++ app/src/main/java/barrera/alejandro/swapi/core/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
-package barrera.alejandro.swapi.ui.theme
+package barrera.alejandro.swapi.core.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun SwapiCambiaTuDietaTheme(
+fun SwapiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
