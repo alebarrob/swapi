0a04d0276952912f3f39a5e3fb772ecf194bb07b
alebarrob <abarrobles@gmail.com>
2024-09-16 19:58:56 +0200

    Unused function deleted

--- app/src/main/java/barrera/alejandro/swapi/food_swap/presentation/mapper/CategoryMapper.kt
+++ app/src/main/java/barrera/alejandro/swapi/food_swap/presentation/mapper/CategoryMapper.kt
@@ -1,18 +1,10 @@
 package barrera.alejandro.swapi.food_swap.presentation.mapper
 
-import androidx.compose.ui.text.capitalize
-import androidx.compose.ui.text.intl.Locale
 import barrera.alejandro.swapi.food_swap.domain.model.Category
 import barrera.alejandro.swapi.food_swap.presentation.model.CategoryUi
 
 fun Category.toCategoryUi() = CategoryUi(
     id = id,
     name = name.uppercase(),
-    conversionFactor = conversionFactor
-)
-
-fun CategoryUi.toCategory() = Category(
-    id = id,
-    name = name.lowercase().capitalize(Locale.current),
     conversionFactor = conversionFactor
 )
