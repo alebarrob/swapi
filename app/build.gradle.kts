b2fb61ec60d72c9956a7a51f38b65ce4e94ae0be
alebarrob <abarrobles@gmail.com>
2024-09-25 15:45:41 +0200

    Version changed to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "barrera.alejandro.swapi"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "barrera.alejandro.swapi.core.util.SwapiTestRunner"
         vectorDrawables {
