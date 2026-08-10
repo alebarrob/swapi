3359a8b82a008fa4966f96efa0a492705a815c79
alebarrob <abarrobles@gmail.com>
2026-08-10 10:08:39 +0200

    Release v1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "barrera.alejandro.swapi"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "barrera.alejandro.swapi.util.SwapiTestRunner"
 
