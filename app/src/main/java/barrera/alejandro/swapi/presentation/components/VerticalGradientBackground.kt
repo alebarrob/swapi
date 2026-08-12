f7497d9a7381f0cdc8a698bb2b82fbfbbc7b1adb
alebarrob <abarrobles@gmail.com>
2026-08-12 12:31:23 +0200

    Delete imports

--- app/src/main/java/barrera/alejandro/swapi/presentation/components/VerticalGradientBackground.kt
+++ app/src/main/java/barrera/alejandro/swapi/presentation/components/VerticalGradientBackground.kt
@@ -3,7 +3,6 @@ package barrera.alejandro.swapi.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
